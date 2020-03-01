6c2fb5dc76de648d245509125442a1e96040e999
Aleksandra Sawczuk <s16446@pjwstk.edu.pl>
2020-03-01 12:52:18 +0100

    Final version from classes

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cw1
 {
    public class Program
     {
-        public static async Task void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var client = new HttpClient(); // var zeby dwa razy nie pisac nazwy typu
             HttpResponseMessage result = await client.GetAsync("https://www.pjwstk.edu.pl");
 
             if (result.IsSuccessStatusCode) {
-
                 string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+                foreach (var m in matches) {
+                    Console.WriteLine(m.ToString());
+                }
             }
+
+            
             Console.WriteLine("Koniec!");
         }
     }
