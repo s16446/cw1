3c1886f7d3e6d1a5c639dc9d435cee42f045ebac
Aleksandra Sawczuk <s16446@pjwstk.edu.pl>
2020-03-01 12:14:25 +0100

    Adding personal info to README.md file

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+   public class Program
+    {
+        public static async Task void Main(string[] args)
+        {
+            var client = new HttpClient(); // var zeby dwa razy nie pisac nazwy typu
+            HttpResponseMessage result = await client.GetAsync("https://www.pjwstk.edu.pl");
+
+            if (result.IsSuccessStatusCode) {
+
+                string html = await result.Content.ReadAsStringAsync();
+            }
+            Console.WriteLine("Koniec!");
+        }
+    }
+}
